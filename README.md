e5455ccf76e914ee77077a43317c0a26c2be1f37
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-27 08:28:16 -0300

    add atividade 2704

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 Repositório dedicado aos desafios de código.
 
 refazer atividades
-01-2667
-02-2620
-03-2704
 04-2665
---
+++ leetcode/task03-2704.js
@@ -0,0 +1,34 @@
+/*
+  Escreva uma função expect que ajude os desenvolvedores a testar seus códigos. Ela deve receber qualquer valor val e retornar um objeto com as duas funções a seguir.
+
+  toBe(val) aceita outro valor e retorna true se os dois valores === são iguais. Se não forem iguais, deve lançar um erro  "Not Equal".
+  
+  notToBe(val) aceita outro valor e retorna true se os dois valores !== são iguais. Se forem iguais, deve lançar um erro  "Equal".
+*/
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: function (expected) {
+      if (expected === val) {
+        return true;
+      } else {
+        throw new Error('Not Equal');
+      }
+    },
+    notToBe: function (expected) {
+      if (expected === val) {
+        return true;
+      } else {
+        throw new Error('Equal');
+      }
+    },
+  };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
