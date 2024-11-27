cfe9a2bcbb2cc5c9443bc56773f90d8f62b400b5
lucasrochabz <lucasbezerrar@gmail.com>
2024-11-27 09:21:38 -0300

    add atividade 2665

---
+++ leetcode/task04-2665.js
@@ -0,0 +1,37 @@
+/*
+  Escreva uma função createCounter. Ela deve aceitar um inteiro inicial init. Ela deve retornar um objeto com três funções.
+
+  As três funções são:
+
+  increment() aumenta o valor atual em 1 e depois o retorna.
+  decrement() reduz o valor atual em 1 e depois o retorna.
+  reset() define o valor atual init e então o retorna.
+*/
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let current = init;
+  return {
+    increment: function () {
+      current = current + 1;
+      return current;
+    },
+    decrement: function () {
+      current = current - 1;
+      return current;
+    },
+    reset: function () {
+      current = init;
+      return current;
+    },
+  };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
