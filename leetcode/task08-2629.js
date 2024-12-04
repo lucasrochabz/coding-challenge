ccb253e14a19c113a0d33df80b18e7fa3e29cf74
lucasrochabz <lucasbezerrar@gmail.com>
2024-12-04 12:16:17 -0300

    add task 2629

--- leetcode/task08-2629.js
+++ leetcode/task08-2629.js
@@ -14,7 +14,19 @@
  */
 
 var compose = function (functions) {
-  return function (x) {};
+  if (functions.length === 0) {
+    return function (x) {
+      return x;
+    };
+  }
+
+  return function (x) {
+    let result = x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result);
+    }
+    return result;
+  };
 };
 
 /**
